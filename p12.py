a55ae1f46abe3be1d91d6bd860085a28d4fc7386
Rahul K B <79784817+RahulKB31@users.noreply.github.com>
2023-08-04 23:08:28 +0100

    Add files via upload

---
+++ p12.py
@@ -0,0 +1,18 @@
+#
+
+'''
+Find maximum of two numbers in python
+'''
+
+
+def maximum(a, b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+
+# Driver code
+a = 2
+b = 3
+print(maximum(a, b))
