613cffff283808c8a724da451682a4e2975afe48
Rahul K B <79784817+RahulKB31@users.noreply.github.com>
2023-08-22 07:18:17 +0100

    Add files via upload

---
+++ p113.py
@@ -0,0 +1,183 @@
+#731
+
+'''
+Python dictionary with keys having multiple inputs
+'''
+
+import random as rn
+
+dict = {}
+
+x,y,z = 10,20,30
+dict[x,y,z] = x+y-z
+
+x,y,z = 5,2,4
+dict[x,y,z] = x+y-z
+
+print(dict)
+
+##############################################################################################################
+
+#732
+
+'''
+Python dictionary with keys having multiple inputs to get access to the keys
+'''
+
+places = {("19.07'53.2", "72.54'51.0"): "Mumbai", ("19.07'53.2", "72.54'51.0"): "Delhi" }
+
+print(places)
+print("\n")
+
+lat = []
+long = []
+plc = []
+for i in places:
+    lat.append(i[0])
+    long.append(i[1])
+    plc.append(places[i[0],i[1]])
+
+print(lat)
+print(long)
+print(plc)
+
+###############################################################################################################
+
+#733
+
+'''
+Python dictionary with keys having multiple inputs
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
