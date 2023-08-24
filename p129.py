4bf8e7318295ed2fe9a79938a8ab2561779915ed
Rahul K B <79784817+RahulKB31@users.noreply.github.com>
2023-08-24 07:08:15 +0100

    Add files via upload

---
+++ p129.py
@@ -0,0 +1,224 @@
+#778
+
+'''
+Python program to create a list of tuples from given list having number and its cube in each tuple
+'''
+
+# Using pow() function
+
+list1 = [1,2,5,6]
+
+res = [(val, pow(val,3)) for val in list1]
+print(res)
+
+##################################################################################################################
+
+#779
+
+'''
+Using ** operator
+'''
+
+val = [1,2,5,6]
+
+res = [(val, val**3) for val in list1]
+print(res)
+
+##################################################################################################################
+
+#780
+
+'''
+Using map() and lambda function
+'''
+
+list1 = [1,2,5,6]
+res = list(map(lambda x: (x,x**3), list1))
+print(res)
+
+###############################################################################################################
+
+#781
+
+'''
+Using a for loop to iterate through the values in the list and create a tuple of each value and its cube
+'''
+
+list1 = [1,2,5,6]
+res = []
+
+for val in list1:
+    tup = (val, val**3)
+    res.append(tup)
+
+print(res)
+
+#################################################################################################################
+
+#782
+
+'''
+Using re method
+'''
+
+import re
+
+lst_str = "1,2,5,6"
+lst = [int(num) for num in re.findall(r'\d+', lst_str)]
+result = [(num, num**3) for num in lst]
+print(result)
+
+##################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
