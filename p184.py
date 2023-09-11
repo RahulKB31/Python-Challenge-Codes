958eea67e41de0c7216057e0ebe39a920c9c7f6f
RAHUL KB <rahulbasavaraj1@gmail.com>
2023-09-11 21:36:38 +0100

    Updated

--- p184.py
+++ p184.py
@@ -4,3 +4,21 @@
 Remove duplicate words from sentence using regular expression
 '''
 
+import re
+
+def removeDuplicateWords(input):
+
+    #regex to matching repeated words
+    regex = r'\b(\w+)(?:\w+\1\b)+'
+
+    return re.sub(regex, r'\1', input, flags=re.IGNORECASE)
+
+#Test case 1:
+str1 = 'Good bye bye world world'
+print(removeDuplicateWords(str1))
+
+str2 = 'Ram went went to to his home'
+print(removeDuplicateWords(str2))
+
+str3 = "Hello hello world world"
+print(removeDuplicateWords(str3))
