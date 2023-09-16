9b0a681e154867d7872a5bb6261800c9f8fbb50b
RAHUL KB <rahulbasavaraj1@gmail.com>
2023-09-16 16:34:17 +0100

    Updated

--- p186.py
+++ p186.py
@@ -1,5 +1,33 @@
-#91
+#924
 
 '''
-
+Python Regex | Program to accept string ending with alphanumeric character
 '''
+
+# re module provides support for regular expressions
+
+import re
+
+# make a regular expression to accept string ending with alphanumeric character
+regex = '[a-zA-Z0-9]$'
+
+def check(string):
+    if(re.search(regex, string)):
+        print("Accept")
+    else:
+        print("Discard")
+
+if __name__ == '__main__':
+    string = "ankirai@"
+    check(string)
+
+    string = "ankitrai326"
+    check(string)
+
+    string = "ankit."
+    check(string)
+
+    string = "geeksforgeeks"
+    check(string)
+
+###################################################################################################################
