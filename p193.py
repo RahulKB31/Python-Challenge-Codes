0f134277a373e15120a5d6f2381b18a734fd5bf4
RAHUL KB <rahulbasavaraj1@gmail.com>
2023-09-17 16:00:56 +0100

    'Updated

--- p193.py
+++ p193.py
@@ -1,5 +1,17 @@
 #936
 
 '''
-
+Python program to find files having a particular extension Regex
 '''
+
+import re
+
+filenames = ["gfg.html","geeks.xml","computer.txt","geeksforgeeks.jpg"]
+
+for file in filenames:
+    match = re.search("\.xml$",file)
+
+    if match:
+        print("The file ending with .xml is:",file)
+
+#################################################################################################################
