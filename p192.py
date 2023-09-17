bbc14ff120b77c91f3aaa204eefba760e23ce8b9
RAHUL KB <rahulbasavaraj1@gmail.com>
2023-09-17 15:52:22 +0100

    Updated

--- p192.py
+++ p192.py
@@ -58,7 +58,30 @@ if __name__ == '__main__':
 
 ###################################################################################################################
 
+#935
 
+'''
+Test if the email address is valid or not in Python using email_validator
+'''
+
+from email_validator import validate_email, EmailNotValidError
+
+def check(email):
+    try:
+        # validate and get info
+        v = validate_email(email)
+
+        # replace with normalized form
+        email = v["email"]
+        print("True")
+    except EmailNotValidError as e:
+        # email is not valid, exception message is human_readable
+        print(str(e))
+
+check("my.ownsite@our-earth.org")
+check("ankitrai326.com")
+
+###################################################################################################################
 
 
 
