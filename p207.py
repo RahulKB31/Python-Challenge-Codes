f7ddbb579b4d18cd5259ef0bdf76b31e4443ed67
RahulKB31 <rahulbasavaraj1@gmail.com>
2023-09-27 23:50:46 +0100

    Updated

---
+++ p207.py
@@ -0,0 +1,41 @@
+#969
+
+'''
+Python - Append content of one text file to another
+'''
+
+firstfile = input("Enter the name of first file")
+secondfile = input("Enter the name of second file")
+
+# opening both the files in read only mode to read initial contents
+f1 = open(firstfile,'r')
+f2 = open(secondfile,'r')
+
+#printing the contents of the file before appending
+print('content of first file before appending', f1.read())
+print('content of the seconf file before appending', f2.read())
+
+#closing the files
+f1.close()
+f2.close()
+
+# opening first file in append mode and second file in read mode
+f1 = open(firstfile,'a+')
+f2 = open(secondfile,'r')
+
+#opening the contents of the second file to the first file
+f1.write(f2.read())
+
+# relocating the cursor of the files at the beginning
+f1.seek(0)
+f2.seek(0)
+
+#printing the contents of the files after appending
+print('content of first file after appending',f1.read())
+print('content of second file after appending',f2.read())
+
+#closing the files
+f1.close()
+f2.close()
+
+#######################################################################################################################
