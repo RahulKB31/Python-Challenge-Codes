0b5466300827e42aeefead66e2241e1e4a9317a9
RahulKB31 <rahulbasavaraj1@gmail.com>
2023-10-02 23:57:26 +0100

    Updated

--- p213.py
+++ p213.py
@@ -56,3 +56,37 @@ llist.printList()
 
 #983
 
+class Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+Class LinkedList:
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    def reverseUtil(self, curr, prev):
+        if curr.next is None:
+            self.head = curr
+
+            # Update next to prev node
+            curr.next = prev
+            return
+
+        # Save curr.next node for recursive call
+        next = curr.next
+
+        # And update next
+        curr.next = prev
+
+        self.reverseUtil(next,curr)
+
+    # This function mainly calls reverseUtil() with previous as None
+    def reverse(self):
+        if self.head is None:
+            return
+        self.reverseUtil(self.head,None)
+
+
