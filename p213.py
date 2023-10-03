25235ede0c44cf85485db0e03ffc05f1c0d7169a
RahulKB31 <rahulbasavaraj1@gmail.com>
2023-10-03 08:51:02 +0100

    Updated

--- p213.py
+++ p213.py
@@ -89,4 +89,37 @@ Class LinkedList:
             return
         self.reverseUtil(self.head,None)
 
+    # Function to insert a new mode at the beginning
+    def push(self, new_data):
+        new_mode = Node(new_data)
+        new_node.next = self.head
+        self.head = new_mode
 
+    # Utility function to print the LinkedList
+    def printList(self):
+        temp = self.head
+        while(temp):
+            print(temp.data,end=" ")
+            temp = temp.next
+
+#Driver program
+llist = LinkedList()
+
+llist.push(8)
+llist.push(7)
+llist.push(6)
+llist.push(5)
+llist.push(4)
+llist.push(3)
+llist.push(2)
+llist.push(1)
+
+print("GIven linked list")
+llist.printList()
+
+llist.reverse()
+
+print("\nReverse linked list")
+llist.printList()
+
+######################################################################################################################
